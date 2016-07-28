abfdfe6c22374ee089df1050c7ce5e2a426d0bcb
Lacau <maz_llacau@uolinc.com>
2016-07-28 20:41:18 -0300

    Add SpringBoot.

--- src/main/java/br/com/java/scripting/groovy/Application.java
+++ src/main/java/br/com/java/scripting/groovy/Application.java
@@ -3,22 +3,24 @@ package br.com.java.scripting.groovy;
 import javax.swing.JDialog;
 
 import br.com.java.scripting.groovy.view.MainDialog;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by lacau on 27/07/16.
  */
+@SpringBootApplication
 public class Application {
 
     /**
      * Launch the application.
      */
     public static void main(String[] args) {
-        try {
-            MainDialog dialog = new MainDialog();
-            dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-            dialog.setVisible(true);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        MainDialog dialog = new MainDialog();
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+        SpringApplication.run(Application.class, args);
+
+        dialog.setVisible(true);
     }
 }
