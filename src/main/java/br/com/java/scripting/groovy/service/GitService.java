4a8f32f3c278a009ed34ec0ea42836fed03c45ce
Lacau <maz_llacau@uolinc.com>
2016-09-15 20:40:49 -0300

    Add new methods to GitService.

--- src/main/java/br/com/java/scripting/groovy/service/GitService.java
+++ src/main/java/br/com/java/scripting/groovy/service/GitService.java
@@ -3,6 +3,8 @@ package br.com.java.scripting.groovy.service;
 import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Repository;
 
@@ -28,6 +30,23 @@ public class GitService {
         createRepository();
     }
 
+    public String getRepositoryPath() {
+        return localRepository.getDirectory().getParent();
+    }
+
+    public void commitFile(File file) {
+        Git git = new Git(localRepository);
+
+        final String fileName = file.getName();
+        try {
+            git.add().addFilepattern(fileName).call();
+            git.commit().setMessage(String.format("Add file '%s'", fileName)).call();
+            System.out.println(String.format("INFO: file '%s' was committed.", fileName));
+        } catch(GitAPIException e) {
+            System.out.println(String.format("ERROR: could not commit file '%s'", fileName));
+        }
+    }
+
     private void createDirectory() {
         File file = new File(PATH);
         if(!file.exists()) {
@@ -39,6 +58,7 @@ public class GitService {
         try {
             localRepository = new FileRepository(PATH + "/.git");
             localRepository.create();
+            System.out.println("INFO: repository created.");
         } catch(IllegalStateException e) {
             System.out.println("WARNING: repository already exists.");
         }
