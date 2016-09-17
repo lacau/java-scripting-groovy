51f1a0950b1ab3dafad85446c07b6399a67da298
lacau <llacau@gmail.com>
2016-09-17 02:51:00 -0300

    Add random move on default cycle implementation.

--- src/main/java/br/com/java/scripting/groovy/Application.java
+++ src/main/java/br/com/java/scripting/groovy/Application.java
@@ -3,8 +3,12 @@ package br.com.java.scripting.groovy;
 import java.io.IOException;
 
 import br.com.java.scripting.groovy.core.MonitorTask;
+import br.com.java.scripting.groovy.factory.MessageFactory;
+import br.com.java.scripting.groovy.factory.MessageType;
 import br.com.java.scripting.groovy.service.GitService;
 import br.com.java.scripting.groovy.view.MainDialog;
+
+;
 
 /**
  * Created by lacau on 27/07/16.
@@ -23,7 +27,7 @@ public class Application {
         try {
             GitService.getInstance().init();
         } catch(IOException e) {
-            System.out.println("ERROR: IOException, application crash.");
+            System.out.println(MessageFactory.createMessage(MessageType.ERROR, "IOException, application crash."));
             MonitorTask.kill();
             System.exit(0);
         }
--- src/main/java/br/com/java/scripting/groovy/core/CycleImpl.java
+++ src/main/java/br/com/java/scripting/groovy/core/CycleImpl.java
@@ -1,6 +1,8 @@
 package br.com.java.scripting.groovy.core;
 
 import java.awt.Color;
+import java.awt.Point;
+import java.util.Random;
 
 /**
  * Created by lacau on 09/08/16.
@@ -9,27 +11,144 @@ public class CycleImpl implements Cycle {
 
     private Geometry geometry;
 
-    private static final long INTERVAL = 50L;
+    private Stage stage;
+
+    private static final long INTERVAL = 2L;
+
+    private static final int DISTANCE = 3;
+
+    private Random random = new Random();
+
+    private int direction;
 
     @Override
     public void init(Stage stage, Geometry geometry) {
+        this.stage = stage;
         this.geometry = geometry;
-        stage.setBackgroundColor(Color.black);
+        this.stage.setBackgroundColor(Color.black);
+        changeColor();
+        changeDirection();
     }
 
     @Override
     public Geometry beforeStep() {
-        geometry.setBorderColor(Color.white);
         return geometry;
     }
 
     @Override
     public Geometry afterStep() {
+        switch(direction) {
+            case 0:
+                return moveDown(DISTANCE);
+            case 1:
+                return moveUp(DISTANCE);
+            case 2:
+                return moveLeft(DISTANCE);
+            case 3:
+                return moveRight(DISTANCE);
+            case 4:
+                moveDown(DISTANCE);
+                return moveRight(DISTANCE);
+            case 5:
+                moveDown(DISTANCE);
+                return moveLeft(DISTANCE);
+            case 6:
+                moveUp(DISTANCE);
+                return moveRight(DISTANCE);
+            case 7:
+                moveUp(DISTANCE);
+                return moveLeft(DISTANCE);
+        }
+
         return geometry;
     }
 
     @Override
     public long getInterval() {
         return INTERVAL;
+    }
+
+    private Geometry moveUp(int distance) {
+        if(canMoveUp(distance))
+            for(Point p : geometry.getPoints())
+                p.y -= distance;
+
+        return geometry;
+    }
+
+    private boolean canMoveUp(int distance) {
+        for(Point p : geometry.getPoints())
+            if(p.y - distance < 0) {
+                changeDirection();
+                changeColor();
+                return false;
+            }
+
+        return true;
+    }
+
+    private Geometry moveDown(int distance) {
+        if(canMoveDown(distance))
+            for(Point p : geometry.getPoints())
+                p.y += distance;
+
+        return geometry;
+    }
+
+    private boolean canMoveDown(int distance) {
+        for(Point p : geometry.getPoints())
+            if(p.y + distance > stage.getHeight()) {
+                changeDirection();
+                changeColor();
+                return false;
+            }
+
+        return true;
+    }
+
+    private Geometry moveLeft(int distance) {
+        if(canMoveLeft(distance))
+            for(Point p : geometry.getPoints())
+                p.x -= distance;
+
+        return geometry;
+    }
+
+    private boolean canMoveLeft(int distance) {
+        for(Point p : geometry.getPoints())
+            if(p.x - distance < 0) {
+                changeDirection();
+                changeColor();
+                return false;
+            }
+
+        return true;
+    }
+
+    private Geometry moveRight(int distance) {
+        if(canMoveRight(distance))
+            for(Point p : geometry.getPoints())
+                p.x += distance;
+
+        return geometry;
+    }
+
+    private boolean canMoveRight(int distance) {
+        for(Point p : geometry.getPoints())
+            if(p.x + distance > stage.getWidth()) {
+                changeDirection();
+                changeColor();
+                return false;
+            }
+
+        return true;
+    }
+
+    private void changeDirection() {
+        direction = random.nextInt(8);
+    }
+
+    private void changeColor() {
+        geometry.setBorderColor(new Color(random.nextFloat(), random.nextFloat(), random.nextFloat()));
     }
 }
